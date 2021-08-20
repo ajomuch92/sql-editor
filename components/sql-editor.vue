f63932adb5ae39491748ad859ca771393154a06f
ajomuch92 <aaronmontes_92@yahoo.es>
2021-08-20 14:44:37 -0600

    Creating sql-editor base on vue-prism-editor and prismjs

---
+++ components/sql-editor.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="sql-editor-container">
+    <client-only placeholder="Loading...">
+      <prism-editor v-model="code" class="my-editor" :highlight="highlighter" line-numbers />
+    </client-only>
+  </div>
+</template>
+
+<script>
+import 'prismjs'
+import 'prismjs/themes/prism-tomorrow.css'
+import 'vue-prism-editor/dist/prismeditor.min.css'
+import { PrismEditor } from 'vue-prism-editor'
+import { highlight, languages } from 'prismjs/components/prism-core'
+import 'prismjs/components/prism-sql'
+
+export default {
+  name: 'SqlEditor',
+  components: {
+    PrismEditor
+  },
+  data: () => ({
+    code: ''
+  }),
+  methods: {
+    highlighter (code) {
+      return highlight(code, languages.sql)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .my-editor {
+    background: #2d2d2d;
+    color: #ccc;
+    font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+    font-size: 14px;
+    line-height: 1.5;
+    padding: 5px;
+    height: 250px;
+  }
+
+  .prism-editor__textarea:focus {
+    outline: none;
+  }
+</style>
