5631a168a61176d3db2dac27f32dfb3d0867413a
ajomuch92 <aaronmontes_92@yahoo.es>
2021-08-21 07:20:03 -0600

    Updating index page to use sql editor and supbase query executor

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,30 @@
 <template>
   <div>
-    <sql-editor v-model="sqlQuery" />
+    <h1>Welcome</h1>
+    <p>This is a basic query builder</p>
+    <sql-editor v-model="sqlQuery" @play-clicked="queryHandler" />
   </div>
 </template>
 
 <script>
 import sqlEditor from '~/components/sql-editor.vue'
+import executeQuery from '~/assets/database/supabase-client'
+
 export default {
   name: 'Index',
   components: { sqlEditor },
   data: () => ({
     sqlQuery: ''
-  })
+  }),
+  methods: {
+    queryHandler ({ tableName }) {
+      executeQuery(tableName).then((data) => {
+        console.log(data)
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+  }
 }
 </script>
 
