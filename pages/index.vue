97d4f2bd89f0e08705840e2797bea03ac87d046e
ajomuch92 <aaronmontes_92@yahoo.es>
2021-08-21 15:38:56 -0600

    Adding vue-lazy-hydration plugin

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
   <div>
     <h1>Welcome</h1>
     <p>This is a basic query builder</p>
-    <sql-editor v-model="sqlQuery" @play-clicked="queryHandler" @clear-clicked="clearHandler" />
+    <LazyHydrate when-visible on-interaction>
+      <sql-editor v-model="sqlQuery" @play-clicked="queryHandler" @clear-clicked="clearHandler" />
+    </LazyHydrate>
     <v-card-title>
       {{ resultTitle }}
       <v-spacer />
@@ -15,41 +17,46 @@
         :disabled="items.length==0"
       />
     </v-card-title>
-    <data-table :items="items" :headers="headers" :loading="loadingData" :search="search" />
-    <v-dialog
-      v-model="errorDialog"
-      max-width="290"
-    >
-      <v-card>
-        <v-card-title class="text-h5">
-          Error
-        </v-card-title>
-        <v-card-text>
-          There was an error during fetching the data
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="red darken-1"
-            text
-            @click="errorDialog = false"
-          >
-            Close
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <LazyHydrate>
+      <data-table :items="items" :headers="headers" :loading="loadingData" :search="search" />
+    </LazyHydrate>
+    <LazyHydrate>
+      <v-dialog
+        v-model="errorDialog"
+        max-width="290"
+      >
+        <v-card>
+          <v-card-title class="text-h5">
+            Error
+          </v-card-title>
+          <v-card-text>
+            There was an error during fetching the data
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              color="red darken-1"
+              text
+              @click="errorDialog = false"
+            >
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </LazyHydrate>
   </div>
 </template>
 
 <script>
+import LazyHydrate from 'vue-lazy-hydration'
 import sqlEditor from '~/components/sql-editor.vue'
 import executeQuery from '~/assets/database/supabase-client'
 import DataTable from '~/components/data-table.vue'
 
 export default {
   name: 'Index',
-  components: { sqlEditor, DataTable },
+  components: { sqlEditor, DataTable, LazyHydrate },
   data: () => ({
     sqlQuery: '',
     loadingData: false,
