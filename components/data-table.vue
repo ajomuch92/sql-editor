631bbfbbbd88ce9dafaf7eb9972087e77ecacf0f
ajomuch92 <aaronmontes_92@yahoo.es>
2021-08-21 14:38:42 -0600

    Creating data table component to show results

---
+++ components/data-table.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="data-table-container">
+    <v-data-table
+      :headers="getHeaderTransformed"
+      :items="items"
+      :items-per-page="10"
+      class="elevation-1"
+      show-group-by
+      :loading="loading"
+      :search="search"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataTable',
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    },
+    headers: {
+      type: Array,
+      default: () => []
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    search: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    getHeaderTransformed () {
+      return this.headers.map(r => ({ text: this._.capitalize(r), value: r }))
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
